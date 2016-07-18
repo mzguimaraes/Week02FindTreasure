0873c9ee2b689e21a1dde5284b1026e2fbdaefb5
Yang Liu <Yang Liu>
2016-07-18 12:44:13 -0400

    Tweak the moveSpeed of the player. Now it is related with deltaTime.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
+    public float moveSpeed = 20f;
 	// Use this for initialization
 	void Start () {
 	    
@@ -12,19 +12,18 @@ public class Player : MonoBehaviour {
 	void Update () {
         GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
-        const float moveSpeed = 7.5f;
         // use W,A,S,D to move the player around
         if (Input.GetKey(KeyCode.W)) {
-            GetComponent<Rigidbody2D>().velocity += new Vector2(0, moveSpeed);
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, moveSpeed) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S)) {
-            GetComponent<Rigidbody2D>().velocity += new Vector2(0, -moveSpeed);
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, -moveSpeed) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A)) {
-            GetComponent<Rigidbody2D>().velocity += new Vector2(-moveSpeed, 0);
+            GetComponent<Rigidbody2D>().velocity += new Vector2(-moveSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D)) {
-            GetComponent<Rigidbody2D>().velocity += new Vector2(moveSpeed, 0);
+            GetComponent<Rigidbody2D>().velocity += new Vector2(moveSpeed, 0) * Time.deltaTime;
         }
     }
 }
