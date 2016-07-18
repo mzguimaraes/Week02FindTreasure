95116f6ebabe5acbe46a68a31b8c7db035023efe
Yang Liu <Yang Liu>
2016-07-17 20:55:26 -0400

    Added basic player movements.

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+        const float moveSpeed = 7.5f;
+        // use W,A,S,D to move the player around
+        if (Input.GetKey(KeyCode.W)) {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(0, -moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(-moveSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            GetComponent<Rigidbody2D>().velocity += new Vector2(moveSpeed, 0);
+        }
+    }
+}
