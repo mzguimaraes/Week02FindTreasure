399f050b5c749b4030dbb234068f4c6b1eacceb2
Marcus Guimaraes <mguimaraes42@gmail.com>
2016-07-23 17:50:25 -0400

    added character mechanics

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+
+	public Text text;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		//check if other is the player
+		if (other.GetComponent<Player>() != null) {
+			//increment player speed
+			other.GetComponent<Player>().moveScalar += 10;
+			text.text = "Character found!  Movement speed increased!";
+			//gameObject.SetActive(false);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		gameObject.SetActive(false);
+	}
+}
