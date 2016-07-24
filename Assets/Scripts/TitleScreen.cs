31fe0afb95259b9238ce69932e0ac1ebe44b0e7f
Marcus Guimaraes <mguimaraes42@gmail.com>
2016-07-24 03:00:01 -0400

    Added control screen

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -4,11 +4,13 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour {
+
+	public int nextScene = 1;
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			SceneManager.LoadScene(1);
+			SceneManager.LoadScene(nextScene);
 		}
 	}
 }
